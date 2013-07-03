be3d1a650add7e939ac4dd227c15b20d07ab994e
dwong <invertedaerials@gmail.com>
2013-07-03 00:20:16 -0600

    Fixed some Fusion import issues.

--- twitter_analysis/script/fusion_import.rb
+++ twitter_analysis/script/fusion_import.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
+require 'rubygems'
 require 'uri'
 require 'net/http'
 require 'net/https'
 require 'mongo'
+require 'logger'
 
 include Mongo
 
@@ -17,8 +19,8 @@ Tasks:
 @client = MongoClient.new('localhost', 27017)
 @db     = @client['undergrad_research']
 @coll   = @db['tweets']
-
-puts "Preparing to retrieve the first 5,000 rows from database.\n\n"
+num_rows = 100
+puts "Preparing to retrieve the first " + num_rows.to_s + " rows from database.\n\n"
 @items = @coll.find({text: { '$exists' => true }, coordinates: { '$exists' => true }, created_at: { '$exists' => true } }, {:fields => ["text", "coordinates", "created_at"]})
 count = 1
 header = "Text,Location,Time"
@@ -26,18 +28,20 @@ data = header + "\n"
 
 # Iterate through rows of the DB
 while @items.has_next?
-  if count >= 5000
+  if count >= num_rows
     break
   end
+  if count % (num_rows/10) == 0
+    puts "Retrieving and formatting rows " + count.to_s + "-" + (count+(num_rows/10)).to_s + "...\n"
+  end
+
   @item =  @items.next()
   correct_coordinates = [@item['coordinates']['coordinates'][1], @item['coordinates']['coordinates'][0]]
   text = @item['text'].to_s
   row = "\"" + text.gsub(/"/,'""') + "\",\"" + correct_coordinates.to_s.gsub(/\[|\]/, '') + "\",\"" + @item['created_at'].to_s + "\""
   data += row + "\n"
+
   count += 1
-  if count % 250 == 0
-    puts "Retrieving and formatting rows " + count.to_s + "-" + (count+250).to_s + "...\n"
-  end
 end
 
 # This code will automatically refresh our access token to make sure we can still access the API
@@ -51,13 +55,15 @@ refresh_token = '' #Needs to be obtained first.
 https = Net::HTTP.new(google_oauth.host, google_oauth.port)
 https.use_ssl = true
 https.verify_mode = OpenSSL::SSL::VERIFY_NONE
-https.set_debug_output $stderr
+https.set_debug_output(Logger.new("http.log"))
 response = https.post(google_oauth.path, 'client_id=' + client_id + '&client_secret=' + client_secret + '&refresh_token=' + refresh_token + '&grant_type=refresh_token')
 
 puts response.body
 =end
 
-auth_code = 'ya29.AHES6ZSreS8FyRF21-2EEerfj__Wp_TWwOKMkDk0ryvZeHc' #This needs to be obtained, not saved
+puts 'Preparing data to be sent to Fusion Tables...'
+
+auth_code = 'ya29.AHES6ZS5GO8Nl1aNU6UObd5vFcMm7ODkeDdn8tmlbboVqNA' #This needs to be obtained, not saved
 
 uri = URI.parse('https://www.googleapis.com/upload/fusiontables/v1/tables/1Lxp4IOOyIpiyCD3LCwc4iS3HHeyQHMFZKGxEI7w/import')
 
@@ -67,7 +73,8 @@ headers = {"Authorization" => 'Bearer ' + auth_code, "Content-Type" => 'applicat
 https = Net::HTTP.new(uri.host, uri.port)
 https.use_ssl = true
 https.verify_mode = OpenSSL::SSL::VERIFY_NONE
-# USE FOR DEBUGGING: https.set_debug_output $stderr
+https.set_debug_output(Logger.new("http.log"))
+puts 'Sending data...'
 response = https.post(uri.path, data, headers)
 
 #Debugging output code:
